6e2444eaf8913c06cfed348a76d31db5a604a829
william <williamwambugu663@gmail.com>
2025-06-18 07:44:09 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -20,19 +20,26 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_list = [bakery.to_dict() for bakery in bakeries]
+    return jsonify(bakeries_list)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
+    bakery = Bakery.query.get(id)
+    return jsonify(bakery.to_dict())
     return ''
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [baked_good.to_dict() for baked_good in baked_goods]
+    return jsonify(baked_goods_list)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return jsonify(baked_good.to_dict())
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
